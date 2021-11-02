28f99565245d85ef7c2add67286b46d0e1d90049
DIR\j.gonzalez.verges <j.gonzalez.verges@accenture.com>
2021-11-02 09:12:50 +0100

    disable submit if the fiels are not fullfilled

--- client/src/features/advertisments/EditAdvForm.js
+++ client/src/features/advertisments/EditAdvForm.js
@@ -4,59 +4,60 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { advEdited } from "./advsSlice";
 
-export const EditAdvForm = ({advId}) => {
-  console.log('MATCH',advId)
+export const EditAdvForm = ({ advId }) => {
+  console.log("MATCH", advId);
   const adv = useSelector((state) =>
-    state.advs.advs.find((advertisement) => String(advertisement.id) === String(advId))
+    state.advs.advs.find(
+      (advertisement) => String(advertisement.id) === String(advId)
+    )
   );
   const [title, setTitle] = useState(adv.title);
   const [valid_until, setValidUntil] = useState(adv.valid_until);
   const [link, setLink] = useState(adv.link);
-
   const dispatch = useDispatch();
+  const canSave =
+  [title, valid_until, link].every(Boolean)
 
   const onTitleChanged = (e) => setTitle(e.target.value);
   const onValidUntilChanged = (e) => setValidUntil(e.target.value);
   const onLinkChanged = (e) => setLink(e.target.value);
 
-  const onSaveAdvClicked =async () => {
-    if (title && valid_until && link && advId) {
-      try{
-       await  dispatch(advEdited({ id: advId, title, valid_until, link }));
-       window.location.reload();
+  const onSaveAdvClicked = async () => {
+    if (canSave) {
+      try {
+        await dispatch(advEdited({ id: advId, title, valid_until, link }));
+        window.location.reload();
       } catch (err) {
         console.error("Failed to edit the advertisement: ", err);
-      } 
+      }
     }
-  }
+  };
   return (
     <section>
-      <h3 className="mt-3 mx-5">Edit Advertisement</h3>
+      <h3 className="mt-1 text-center">Edit Advertisement</h3>
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Advertisment</Form.Label>
+          <Form.Label>Advertisment: {title} </Form.Label>
           <Form.Control
             type="text"
             name="advTitle"
             value={title}
             onChange={onTitleChanged}
-            placeholder="Enter advertisement"
           />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>valid_until</Form.Label>
+          <Form.Label>valid_until: {valid_until}</Form.Label>
           <Form.Control
             type="date"
             name="advValid"
             value={valid_until}
             onChange={onValidUntilChanged}
-            placeholder="valid_until"
           />
           <Form.Text className="text-muted">valid_until</Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Link</Form.Label>
+          <Form.Label>Link: {link}</Form.Label>
           <Form.Control
             type="text"
             value={link}
@@ -65,7 +66,7 @@ export const EditAdvForm = ({advId}) => {
           />
         </Form.Group>
 
-        <Button variant="primary" onClick={onSaveAdvClicked}>
+        <Button variant="primary" disabled={!canSave} onClick={onSaveAdvClicked}>
           Save Advertisement
         </Button>
       </Form>
