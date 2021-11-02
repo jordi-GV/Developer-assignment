b741c1f825fbf048ac6cc4e5f24ca9de511949b3
DIR\j.gonzalez.verges <j.gonzalez.verges@accenture.com>
2021-11-02 08:45:07 +0100

    added delete

--- client/src/features/advertisments/SingleAdvPage.js
+++ client/src/features/advertisments/SingleAdvPage.js
@@ -1,16 +1,28 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
 import Button from "react-bootstrap/Button";
 import { EditAdvForm } from "./EditAdvForm";
+import { useDispatch, useSelector } from "react-redux";
 
-
+import { deleteAdv } from "./advsSlice";
 
 export const SingleAdvPage = ({ match }) => {
+  const dispatch = useDispatch();
   const [toggleForm, setTog] = useState(false);
   const { advId } = match.params;
   const advertisement = useSelector((state) =>
     state.advs.advs.find((a) => String(a.id) === String(advId))
   );
+
+  const onDeleteClicked = async (advId) => {
+    if (advId) {
+      try {
+        await dispatch(deleteAdv(advId)).unwrap();
+        window.location.reload();
+      } catch (err) {
+        console.error("Failed to delete the advertisement: ", err);
+      }
+    }
+  };
 
   const toggle = () => setTog(!toggleForm);
 
@@ -18,7 +30,7 @@ export const SingleAdvPage = ({ match }) => {
     console.log("not found=>" + advId, advertisement);
     return (
       <section>
-        <h2>Advertisement not found!</h2>
+        <h6 className="my-5">Advertisement deleted or not found!</h6>
       </section>
     );
   }
@@ -27,10 +39,19 @@ export const SingleAdvPage = ({ match }) => {
       <article className="advertisement mt-3">
         <h2>{advertisement.title}</h2>
         <p>Title: {advertisement.title}</p>
-        <p >Valid until: {advertisement.link}</p>
-        <p >Link: {advertisement.link}</p>
+        <p>Valid until: {advertisement.link}</p>
+        <p>Link: {advertisement.link}</p>
       </article>
-      <Button onClick={toggle}>Edit</Button>
+      <Button className="m-2 btn-sm"  onClick={toggle}>
+        Edit
+      </Button>
+      <Button
+        className="m-2 btn-sm"
+        variant="danger"
+        onClick={() => dispatch(onDeleteClicked(advertisement.id))}
+      >
+        Delete
+      </Button>
 
       {toggleForm === false ? "" : <EditAdvForm advId={advId} />}
     </section>
